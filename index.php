02ef87c8ac00918f725734d02773dc66a05ba641
Julio Migliari <61835411+Vennomouz@users.noreply.github.com>
2021-09-26 19:36:01 -0300

    arquivos

---
+++ index.php
@@ -0,0 +1,34 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link href="fontawesome/css/all.css" rel="stylesheet">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<body>
+    <div id="login">
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="" method="post">
+							<div class="img-profile"><i class="far fa-user-circle"></i></div>							
+                            <div class="form-group">
+                                <label for="username" class="text-info">Usuário:</label><br>
+                                <input type="text" name="username" id="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Senha:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group submitBtn">
+                                <input type="submit" name="submit" class="btn btn-md" value="Entrar"><br><br>
+								<p class="text-info">Esqueci minha senha</p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
