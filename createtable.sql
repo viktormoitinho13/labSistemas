a08a01db9bb07bda2d2775abaf478339849690b0
viktor moitinho <52150105+viktormoitinho13@users.noreply.github.com>
2021-10-10 15:46:42 -0300

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -24,7 +24,7 @@ create table funcionario (
 create table cargo (
   id_cargo int auto_increment primary key,
   cargo_funcao varchar (30),
-  cargo_loja int,
+  cargo_num_loja int,
   cargo_salario float,
   constraint fk_cargo_funcionario foreign key (id_cargo) references funcionario (id_funcionario)
 )
@@ -39,3 +39,47 @@ create table login (
   loja_login int unique,
   loja_login_senha varchar(100)
 )
+
+/*Descrição da tabela produto 
+	id_produto é a chave primaria auto incrementada da tabela, é um inteiro.
+    ean_produto é um campo unico que armazena o código de barras
+    nome_produto é a descrição do produto
+    preco_produto é o preço de venda
+    custo_produto é o preço de compra
+    fabricanção_produto é a data de fabricação indicada pela fabricante
+    validade_produto é a data de validade indicada pela fabricante
+
+*/
+
+create table produto 
+(
+	id_produto int auto_increment primary key,
+  	ean_produto int unique,
+  	nome_produto varchar (30),
+  	preco_produto float,
+  	custo_produto float,
+  	fabricacao_produto date,
+  	validade_produto date
+)
+
+
+/*abela fabricante
+	id_fabricante auto incrementada e unica que identifica a fabricante
+    cnpj_fabricante campo unico que indetifica o código fiscal da fabricante
+    nome_fabricante é o nome da fabricante
+    telefone_fabricante é o campo que armazena o contato telefonico
+    email_fabricante é o campo que armazena o contato de correio eletronico 
+    
+
+*/
+
+create table fabricante 
+(
+	id_fabricante int auto_increment primary key,
+  	cnpj_fabricante varchar(18),
+  	nome_fabricante varchar (30),
+  	telefone_fabricante varchar(20),
+  	email_fabricante varchar(30),
+  	constraint fk_fabricante_produto foreign key (id_fabricante) references produto (id_produto)
+)
+
